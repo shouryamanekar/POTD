9aa04695c181cfbb7a170bcd64bb62f6d2dbec06
shouryamanekar <speedk143@gmail.com>
2024-12-27 13:08:31 +0530

    27_12_24

---
+++ 24_12_27 Count pairs with given sum/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    int countPairs(int arr[], int target) {
+        // Your code here
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        
+        int count = 0;
+
+        for (int num : arr) {
+            int complement = target - num;
+
+            if (freqMap.containsKey(complement)) {
+                count += freqMap.get(complement);
+            }
+
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        return count;
+    }
+}
